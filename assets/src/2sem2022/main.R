d4c973eb6298676e4c19a01fc8f243d4ee322734
Gilberto Sassi <sassi.pereira.gilberto@gmail.com>
2022-08-07 15:03:21 -0300

    criei páginas para mat236 e matc65 para o segundo semestre de 2022

---
+++ assets/src/2sem2022/main.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+#------------------------------------------------------------------------------
+# mat236
+rmarkdown::render("assets/src/2sem2022/mat236.Rmd",
+                output_dir = "content/ensino/segundo-semestre-de-2022/")
+
+#------------------------------------------------------------------------------
+# matc65
+rmarkdown::render("assets/src/2sem2022//matc65.Rmd",
+                output_dir = "content/ensino/segundo-semestre-de-2022/")
