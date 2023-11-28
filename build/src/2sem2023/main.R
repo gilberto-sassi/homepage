faee21bd43f3629184d1b1f00d96ebb80fb0cf98
Gilberto Pereira Sassi <sassi.pereira.gilberto@gmail.com>
2023-11-28 18:43:15 -0300

    inclusão dos códigos de quarto para matd39, e adoção de quarto para produzir páginas web do site

--- build/src/2sem2023/main.R
+++ build/src/2sem2023/main.R
@@ -1,12 +1,25 @@
-library(rmarkdown)
+library(quarto)
 library(tidyverse)
+
+renderizar <- function(input, output_dir) {
+    quarto::quarto_render(input)
+    name <- str_replace(input, ".qmd", "") |>
+        str_split_1("/") |>
+        last()
+    dir_input <- str_replace(input, paste0(name, ".qmd"), "")
+    file.remove(paste0(output_dir, name, ".html"))
+    file.copy(
+        from = paste0(dir_input, name, ".html"),
+        to = paste0(output_dir, name, ".html")
+    )
+    file.remove(paste0(dir_input, name, ".html"))
+}
 
 #------------------------------------------------------------------------------
 # mat223
-rmarkdown::render("assets/src/2sem2023/mat223.Rmd",
-                output_dir = "content/ensino/segundo-semestre-de-2023")
+renderizar("assets/src/2sem2023/mat223.qmd", "content/ensino/segundo-semestre-de-2023/")
 
 #------------------------------------------------------------------------------
 # matd39
-rmarkdown::render("assets/src/2sem2023/matd39.Rmd",
-                output_dir = "content/ensino/segundo-semestre-de-2023")
+renderizar("assets/src/2sem2023/matd39.qmd",
+               "content/ensino/segundo-semestre-de-2023/")
