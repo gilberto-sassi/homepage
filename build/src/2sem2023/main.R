2b69dd32652640db28c8a850625583696bc04167
Gilberto Pereira Sassi <sassi.pereira.gilberto@gmail.com>
2023-11-29 20:08:03 -0300

    correção de datas

--- build/src/2sem2023/main.R
+++ build/src/2sem2023/main.R
@@ -11,6 +11,7 @@ renderizar <- function(input, output_dir) {
     name <- str_replace(input, ".qmd", "") |>
         str_split_1("/") |>
         last()
+        
     dir_input <- str_replace(input, paste0(name, ".qmd"), "")
     file.remove(paste0(output_dir, name, ".html"))
     file.copy(
@@ -22,9 +23,10 @@ renderizar <- function(input, output_dir) {
 
 #------------------------------------------------------------------------------
 # mat223
-renderizar("assets/src/2sem2023/mat223.qmd", "content/ensino/segundo-semestre-de-2023/")
+renderizar("assets/src/2sem2023/mat223.qmd",
+       "content/ensino/segundo-semestre-de-2023/")
 
 #------------------------------------------------------------------------------
 # matd39
 renderizar("assets/src/2sem2023/matd39.qmd",
-               "content/ensino/segundo-semestre-de-2023/")
+       "content/ensino/segundo-semestre-de-2023/")
