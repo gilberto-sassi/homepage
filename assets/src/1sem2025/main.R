5da07895644db3062f86a617189546e4262e0a1c
Gilberto Pereira Sassi <sassi.pereira.gilberto@gmail.com>
2025-03-20 20:12:11 -0300

    slides, listas de exercícios e outros materiais para o primeiro semestre de 2025

---
+++ assets/src/1sem2025/main.R
@@ -0,0 +1,32 @@
+if (!require(pacman)) {
+    install.packages("pacman")
+    library(pacman)
+}
+
+p_load(glue, quarto)
+p_load(tidyverse)
+
+renderizar <- function(input, output_dir) {
+    quarto_render(input)
+    name <- str_replace(input, ".qmd", "") |>
+        str_split_1("/") |>
+        last()
+        
+    dir_input <- str_replace(input, paste0(name, ".qmd"), "")
+    file.remove(paste0(output_dir, name, ".html"))
+    file.copy(
+        from = paste0(dir_input, name, ".html"),
+        to = paste0(output_dir, name, ".html")
+    )
+    file.remove(paste0(dir_input, name, ".html"))
+}
+
+#------------------------------------------------------------------------------
+# mat020
+renderizar("assets/src/1sem2025/mat236.qmd",
+       "content/ensino/primeiro-semestre-de-2025/")
+
+#------------------------------------------------------------------------------
+# mat022
+renderizar("assets/src/1sem2025/mata44.qmd",
+       "content/ensino/primeiro-semestre-de-2025/")
